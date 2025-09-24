8d859ff8d62459588edaf3acc2f5c747c6f49596
Noel Schneuwly <noel.schneuwly@bluewin.ch>
2025-09-24 20:42:59 +0200

    basic app architecture

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask, jsonify, render_template, request
+
+app = Flask(__name__)
+
+@app.route("/", methods=["GET"])
+def home():
+    return render_template("home.html")
+
+@app.route("/about", methods=["GET"])
+def about():
+    return render_template("about.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=False)
